4d41ed960117b402e3640333045d1514d1691e4c
ZRMYDYCG <547471919@qq.com>
2025-05-15 10:49:00 +0800

    fix: 🪲️packages修复

--- packages/monito-admin/src/main.tsx
+++ packages/monito-admin/src/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '@/styles/reset.less'
-import '@/assets/iconfont/iconfont.less'
-import '@/assets/fonts-input/font.less'
-import '@/styles/common.less'
-import ReactApp from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@/styles/reset.less";
+import "@/assets/iconfont/iconfont.less";
+import "@/assets/fonts-input/font.less";
+import "@/styles/common.less";
+import ReactApp from "./App";
+import { initForReact } from "@monito-project/monito-sdk/src";
 
-// console.log('env环境:\n', import.meta.env)
+console.log("env环境:\n", import.meta.env);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+initForReact();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <ReactApp />,
   // </React.StrictMode>
-)
+);
