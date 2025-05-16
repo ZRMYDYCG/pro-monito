84b5de351d740f8b6628b447dcbfeb23a1e9b2d3
ZRMYDYCG <547471919@qq.com>
2025-05-16 20:01:35 +0800

    feat: 引入jest，启动TDD开发模式

---
+++ packages/monito-sdk/__tests__/track/listeners/index.test.ts
@@ -0,0 +1,7 @@
+describe('trackEvent', () => {
+  it('应该正确收集事件数据', () => {
+    let result = {}
+    expect(result).toEqual({
+    })
+  })
+})
---
+++ packages/monito-sdk/jest.config.ts
@@ -0,0 +1,20 @@
+/*
+ * @Author: ZRMYDYCG
+ * @Date: 2025-05
+ * @LastEditors: ZRMYDYCG
+ * @LastEditTime: 2025-05
+ * @Description: Jest配置文件
+ */
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest', // 使用ts-jest处理TypeScript
+  testEnvironment: 'node', // 测试环境为Node.js
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'], // 支持的模块扩展名
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)'], // 测试文件匹配模式
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest' // 用ts-jest转换TS/TSX文件
+  }
+};
+
+export default config
