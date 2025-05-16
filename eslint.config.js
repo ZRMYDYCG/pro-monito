e0eb434e5f3597e3203149c8cfab02cd11c1f046
ZRMYDYCG <547471919@qq.com>
2025-05-17 07:34:02 +0800

    ci: eslint adjust

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,21 @@
-import eslint from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tseslintParser from '@typescript-eslint/parser';
-import prettier from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslint from '@eslint/js'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tseslintParser from '@typescript-eslint/parser'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import globals from 'globals'
 
 export default [
   {
     files: ['**/*.ts', '**/*.js'],
     languageOptions: {
+      env: {
+        browser: true, // to enable browser globals like indexedDB
+      },
       globals: {
         ...globals.node, // 引入 Node.js 的标准全局变量 (包括 console, process 等)
         ...globals.jest, // 声明Jest测试框架的全局变量（describe/it/expect等）
+        IDBDatabase: 'readonly',
       },
       parser: tseslintParser, // TypeScript 解析器也能解析 JavaScript
       parserOptions: {
@@ -31,7 +35,7 @@ export default [
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': 'off'
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 ]
